898b522355f658fae60b31180357f278814f9ba7
taku-y <taku.yoshioka.4096@gmail.com>
2016-10-12 12:32:34 +0900

    Add notebooks and setting files

---
+++ scripts/create_testenv.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e # fail on first error
+
+PYTHON_VERSION=${PYTHON_VERSION:-3.5} # if no python specified, use 3.5
+
+conda create -n testenv --yes pip python=${PYTHON_VERSION}
+
+source activate testenv
+
+conda install --yes pyqt=4.11.4 jupyter pyzmq numpy scipy nose matplotlib pandas Cython patsy statsmodels joblib coverage
+if [ ${PYTHON_VERSION} == "2.7" ]; then
+  conda install --yes mock enum34;
+fi
+
+pip install tqdm
+pip install --no-deps numdifftools
+pip install git+https://github.com/Theano/Theano.git
+pip install git+https://github.com/mahmoudimus/nose-timer.git
+pip install pymc3
+
+python setup.py build_ext --inplace
